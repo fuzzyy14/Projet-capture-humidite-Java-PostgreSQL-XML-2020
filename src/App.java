64a6c0b066f44e94b2a7b958b2857178b6c30868
FuZzyy14 <simon.delarue2@gmail.com>
2020-09-30 19:34:07 +0200

    Regroupement des 3 DAO de la vue bureau dans un seul pour alléger
    le code

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 
-import donnee.jourDAO;
+import donnee.HumiditeDAO;
 import vue.Fenetre;
 
 public class App {
 
 	public static void main(String[] parametres) {
 		Fenetre.launch(Fenetre.class, parametres);	
-		jourDAO testParsingXML = new jourDAO();
+		HumiditeDAO testParsingXML = new HumiditeDAO();
 		testParsingXML.DecoderXMLJour();
 	}
 
