d32f110addf7ffae09c5dd8255e2d8af0d9b0454
Simon Delarue <51883276+simondelarue14@users.noreply.github.com>
2021-02-13 16:52:13 -0500

    #1 Refonte du style humidité + ajout du fichier sql de la table + début modification du service de données (suppression de la réécriture des urls)

--- SERVICE/404page.php
+++
@@ -1,9 +0,0 @@
-<DOCTYPE htlm>
-<html>
- <head>
-  <title> Normandie Services </title>
- </head>
-  <body>
-  		<h1> Oups fausse route... :( </h1>	
-  </body>
-</html>
---
+++ service-humidite/index.php
@@ -0,0 +1,9 @@
+<?php
+    echo '<a href="../service-humidite/jour.php"> devoircapture.ddns.net/humidite/bureau/jour </a><br>';
+    echo '<a href="../service-humidite/mois.php"> devoircapture.ddns.net/humidite/bureau/mois </a><br>';
+    echo '<a href="../service-humidite/annee.php"> devoircapture.ddns.net/humidite/bureau/annee </a><br>';
+    echo '<a href="../service-humidite/apercu.php"> devoircapture.ddns.net/humidite/mobile/apercu </a><br>';
+    echo '<a href="../service-humidite/alerte.php"> devoircapture.ddns.net/humidite/mobile/alerte </a><br>';
+?>
+
+
